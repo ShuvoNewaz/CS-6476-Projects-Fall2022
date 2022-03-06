6160ad225885fa41391d561ef9b70ce755390232
Tohwx <weixiongtoh@gmail.com>
2022-03-05 23:34:24 -0500

    conda environment.yml and install.sh

---
+++ conda/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ENVIRONMENT_FILE=environment.yml
+echo "Using ${ENVIRONMENT_FILE} ..."
+
+# Ensure mamba is installed.
+conda install -c conda-forge mamba
+
+# Create library environment.
+mamba env create -f "${SCRIPT_DIR}/${ENVIRONMENT_FILE}" \
+&& eval "$(conda shell.bash hook)" \
+&& conda activate cv_proj4 \
+&& python -m pip install -e "$SCRIPT_DIR/.."
