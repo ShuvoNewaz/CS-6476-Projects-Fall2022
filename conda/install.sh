28865bfa4aeb25a37e7b10d85d6d5fcaeba2a62b
Smith, Otis <otis.smith@gatech.edu>
2022-10-03 21:07:45 -0400

    switch mamba to conda in install script

--- conda/install.sh
+++ conda/install.sh
@@ -5,11 +5,8 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ENVIRONMENT_FILE=environment.yml
 echo "Using ${ENVIRONMENT_FILE} ..."
 
-# Ensure mamba is installed.
-conda install -c conda-forge mamba
-
 # Create library environment.
-mamba env create -f ${SCRIPT_DIR}/${ENVIRONMENT_FILE} \
+conda env create -f ${SCRIPT_DIR}/${ENVIRONMENT_FILE} \
 && eval "$(conda shell.bash hook)" \
 && conda activate cv_proj3 \
 && python -m pip install -e $SCRIPT_DIR/..
